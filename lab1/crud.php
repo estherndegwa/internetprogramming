8ee35baea305d79360df89e855dd3d4d9d3f2f39
estherndegwa <54438132+estherndegwa@users.noreply.github.com>
2020-07-17 21:57:14 +0300

    Add files via upload

---
+++ lab1/crud.php
@@ -0,0 +1,16 @@
+<?php
+    interface Crud{
+        public function save();
+        public function readAll();
+        public function readUnique();
+        public function search();
+        public function update();
+        public function removeOne();
+        public function removeAll();
+
+        //we added these methods for lab 2
+        public function validateForm();
+        public function createFormErrorSessions();
+        public function isUserExist();
+    }
+?>
